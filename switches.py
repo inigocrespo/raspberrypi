67c41a9a2b5728bf87176b86f854070d6028bb3f
hake0pi <hake0pi@gmail.com>
2014-03-29 17:34:15 +0000

    switches.py

---
+++ switches.py
@@ -0,0 +1,23 @@
+import time
+import RPi.GPIO as io
+
+io.setmode(io.BCM)
+
+pir_pin = 18
+door_pin = 23
+
+io.setup(pir_pin, io.IN)
+io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)
+
+count = 0;
+
+while True:
+	if io.input(pir_pin):
+		count += 1;
+		print("PIR ALARM (" + str(count) + ")! ")
+
+	if io.input(door_pin):
+		print("DOOR ALARM!")
+	
+	time.sleep(0.5)
+
